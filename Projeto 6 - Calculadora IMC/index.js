a28b41e3740e3c1a5da13749fac1196acfb33eb7
HenriqueVasques <156234428+HenriqueVasques@users.noreply.github.com>
2024-03-05 11:35:37 -0300

    .

--- Projeto 6 - Calculadora IMC/index.js
+++ Projeto 6 - Calculadora IMC/index.js
@@ -2,29 +2,34 @@ const form = document.getElementById('form');
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
-
- 
-
+        
     const height = document.getElementById('height').value;
-    const weight = documet.getElementById('weight').value;
+    const weight = document.getElementById('weight').value;
 
- 
-    const bmi = weight / (height * height).toFixed(2); 
+    const bmi = (weight / (height * height)).toFixed(2); 
 
     const value = document.getElementById('value');
-    let description ='';
-    
-    document.getElementById('infos').classList.remove('hide');
-    document.getElementById('resultado').classList.remove('resultado');
-   
-    
+    let description = '';
+  
+    value.classList.add(attention)
+    document.getElementById('infos').classList.remove('hidden');
 
-
-   
-    if(bmi < 18.5){
-        description = 'Cuidado! Você está abaixo do peso!'
+    if(bmi < 18.5) {
+        description = 'cuidado! Você está abaixo do peso!';
+    } else if(bmi >= 18.5 && bmi <= 25){
+        description = 'Você está no peso ideal!';
+    } else if(bmi >= 25 && bmi <= 30){
+        description = 'Cuidado! Você está com sobreso!';
+        value.classList.remove('attention');
+        value.classList.add('normal');
+    } else if(bmi >= 30 && bmi <= 35){
+        description = 'Cuidado! Você está com obesidade moderada!';
+    } else if(bmi >= 35 && bmi <= 40){
+        description = 'Cuidado! Você está com obesidade severa!';
+    } else{
+        description = 'Cuidado! Você está com obesidade morbida!';
     }
-        value.textContent = bmi
-        document.getElementById('description').textContent = description
-});
 
+    value.textContent = bmi.replace('.', ',');
+    document.getElementById('description').textContent = description;
+  });
